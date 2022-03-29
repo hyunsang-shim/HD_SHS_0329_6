dd7b6c06d09a9452e4cd020040baf3db1ae7eb16
hyunsang-shim <shimhyunsang.dev@gmail.com>
2022-03-29 23:43:24 +0900

    혼자해보기6번
    혼자해보기6번 풀이.
    devkoboso.com/403

--- Test_6/Program.cs
+++ Test_6/Program.cs
@@ -4,9 +4,28 @@ namespace Test_6
 {
     class Program
     {
+        // 6. 반복문을 이용하여 아래 별을 출력하세요
+        // *   *   (별ㅁㅁㅁ별)
+        //  * *    (ㅁ별ㅁ별)
+        //   *      (ㅁㅁ별)
+        //  * *    (ㅁ별ㅁ별)
+        // *   *   (별ㅁㅁㅁ별)
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == j || (i + j == 4))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }            
         }
     }
 }

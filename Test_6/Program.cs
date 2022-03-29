5c53f6fa842062485033b7a887784c2eab6cf066
hyunsang-shim <shimhyunsang.dev@gmail.com>
2022-03-29 22:07:35 +0900

    project is set

---
+++ Test_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
